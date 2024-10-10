b1ce519879526af51edbe026ad7de4e4a6ef6bf0
Frank <fcarpio113@gmail.com>
2024-10-10 07:39:13 -0500

    fist

---
+++ src/main/java/com/envios/proyectoenvios/ProyectoEnviosApplication.java
@@ -0,0 +1,13 @@
+package com.envios.proyectoenvios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/envios/proyectoenvios/ProyectoEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.envios.proyectoenvios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
